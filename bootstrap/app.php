279352afe469fe58535b93aa4820d9a0d9675035
Marco A. Gallegos <ma_galeza@hotmai.com>
2019-04-18 00:31:52 -0500

    update : dependencies & env
    the lumen-framework is updated to 5.8 to take adventage of carbon 2.x,
    the dotenv dependency is updated to 3.x and a constructor call refactor
    was needed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,8 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 try {
-    (new Dotenv\Dotenv(__DIR__.'/../'))->load();
+    $env = Dotenv\Dotenv::create(__DIR__."/../");
+    $env->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     //
 }
